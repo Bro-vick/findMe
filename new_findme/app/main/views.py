be21e3f1733d571ae5a91c79f6f203ea94365282
AYcrown77 <aycrown77@gmail.com>
2023-03-09 20:25:12 +0100

    Change the user route

--- new_findme/app/main/views.py
+++ new_findme/app/main/views.py
@@ -15,7 +15,7 @@ def index():
 def home():
     return render_template('index.html')
 
-@main.route('/user/<username>')
+@main.route('/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
