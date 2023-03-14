da5f9ceee33223eb1e44c7819874be3075c0d403
Adewole Conde <phatboislym@protonmail.com>
2023-03-14 10:06:27 +0000

    update files

--- new_findme/flasky.py
+++ new_findme/flasky.py
@@ -25,5 +25,5 @@ def test(test_names):
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+	app.run(debug=True, host='0.0.0.0', port=5000)
+  
